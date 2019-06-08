3ad034610f8b57d8a86cd95069381735e7b556be
Daniel Barclay <daniel@urbanintel.io>
2019-06-08 13:21:41 +0100

    Ability to add new quotes and have them appear within the Quote Grid

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="container">
     <h1>Hello World</h1>
-    <quote-grid :quotes="quotes"/>
+    <new-quote @quoteAdded="newQuote" />
+    <quote-grid :quotes="quotes" />
   </div>
 </template>
 
 <script>
-  import QuoteGrid from './components/QuoteGrid';
+import QuoteGrid from './components/QuoteGrid';
+import NewQuote from './components/NewQuote';
 export default {
   name: 'app',
   components: {
     QuoteGrid,
+    NewQuote,
   },
   data: () => ({
-    quotes: [
-            'You may say I\'m a dreamer;\n' +
-            'But I\'m not the only one\n'
-    ],
+    quotes: ["You may say I'm a dreamer;\n" + "But I'm not the only one"],
     maxQuotes: 10,
   }),
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    },
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
